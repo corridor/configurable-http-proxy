68b768ebe89b2afdab3416081cb60f0333d4dae6
AbdealiJK <abdealikothari@gmail.com>
2021-07-06 18:37:16 +0530

    store: Add base store class

---
+++ jupyterhub_python_proxy/store.py
@@ -0,0 +1,61 @@
+import typing
+
+from jupyterhub_python_proxy.trie import URLTrie, trim_prefix
+
+
+class BaseStore:
+    def get_target(self, path):
+        raise NotImplementedError(f"{self}: get_target() not implemented")
+
+    def get_all(self):
+        raise NotImplementedError(f"{self}: get_all() not implemented")
+
+    def add(self, path, data):
+        raise NotImplementedError(f"{self}: add() not implemented")
+
+    def update(self, path, data):
+        raise NotImplementedError(f"{self}: update() not implemented")
+
+    def remove(self, path):
+        raise NotImplementedError(f"{self}: remove() not implemented")
+
+    def get(self, path):
+        path = self.clean_path(path)
+        return self.get_all()[path]
+
+    def clean_path(self, path: str):
+        return trim_prefix(path)
+
+
+class MemoryStore(BaseStore):
+    def __init__(self):
+        super().__init__()
+        self.routes: typing.Dict[str, URLTrie] = {}
+        self.urls = URLTrie()
+
+    def get(self, path: str):
+        return self.routes.get(self.clean_path(path))
+
+    def get_target(self, path: str):
+        return self.urls.get(path)
+
+    def get_all(self):
+        return self.routes
+
+    def add(self, path: str, data):
+        path = self.clean_path(path)
+        self.routes[path] = data
+        self.urls.add(path, data)
+
+    def update(self, path: str, data):
+        self.routes[self.clean_path(path)].update(data)
+
+    def remove(self, path: str):
+        path = self.clean_path(path)
+        if path in self.routes:
+            route = self.routes.get(path)
+            del self.routes[path]
+        else:
+            route = None
+        self.urls.remove(path)
+        return route
---
+++ jupyterhub_python_proxy_test/test_store.py
@@ -0,0 +1,75 @@
+from jupyterhub_python_proxy.store import MemoryStore
+
+
+class TestMemoryStore:
+    def setup_method(self, method):
+        self.subject = MemoryStore()
+
+    def test_get(self):
+        self.subject.add("/myRoute", {"test": "value"})
+        route = self.subject.get("/myRoute")
+        assert route == {"test": "value"}
+
+    def test_get_with_invalid_path(self):
+        route = self.subject.get("/wut")
+        assert route is None
+
+    def test_get_target(self):
+        self.subject.add("/myRoute", {"target": "http://localhost:8213"})
+        target = self.subject.get_target("/myRoute")
+        assert target.prefix == "/myRoute"
+        assert target.data["target"] == "http://localhost:8213"
+
+    def test_get_all(self):
+        self.subject.add("/myRoute", {"test": "value1"})
+        self.subject.add("/myOtherRoute", {"test": "value2"})
+
+        routes = self.subject.get_all()
+        assert len(routes) == 2
+        assert routes["/myRoute"] == {"test": "value1"}
+        assert routes["/myOtherRoute"] == {"test": "value2"}
+
+    def test_get_all_with_no_routes(self):
+        routes = self.subject.get_all()
+        assert routes == {}
+
+    def test_add(self):
+        self.subject.add("/myRoute", {"test": "value"})
+
+        route = self.subject.get("/myRoute")
+        assert route == {"test": "value"}
+
+    def test_add_overwrite(self):
+        self.subject.add("/myRoute", {"test": "value"})
+        self.subject.add("/myRoute", {"test": "updatedValue"})
+
+        route = self.subject.get("/myRoute")
+        assert route == {"test": "updatedValue"}
+
+    def test_update(self):
+        self.subject.add("/myRoute", {"version": 1, "test": "value"})
+        self.subject.update("/myRoute", {"version": 2})
+
+        route = self.subject.get("/myRoute")
+        assert route["version"] == 2
+        assert route["test"] == "value"
+
+    def test_remove(self):
+        self.subject.add("/myRoute", {"test": "value"})
+        self.subject.remove("/myRoute")
+
+        route = self.subject.get("/myRoute")
+        assert route is None
+
+    def test_remove_with_invalid_route(self):
+        # No error should occur
+        self.subject.remove("/myRoute/foo/bar")
+
+    def test_has_route(self):
+        self.subject.add("/myRoute", {"test": "value"})
+        route = self.subject.get("/myRoute")
+        assert route == {"test": "value"}
+
+    def test_has_route_path_not_found(self):
+        route = self.subject.get("/wut")
+        assert route is None
