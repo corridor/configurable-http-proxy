8983d1e3be653aa2f328c6084854c7505a4810a4
AbdealiJK <abdealikothari@gmail.com>
2021-07-07 14:26:41 +0530

    __init__: Add logger

--- jupyterhub_python_proxy/__init__.py
+++ jupyterhub_python_proxy/__init__.py
@@ -1,1 +1,10 @@
+import logging
+
 from jupyterhub_python_proxy._version import version as __version__  # noqa: F401
+
+log = logging.getLogger("jupyterhub_python_proxy")
+log.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('[%(levelname)-.1s %(asctime)s %(name)s] %(message)s'))
+logging.root.addHandler(handler)
