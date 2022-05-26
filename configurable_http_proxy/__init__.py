600aab75b537087f656e4a13f4acd34049b6b62f
AbdealiJK <abdealikothari@gmail.com>
2022-05-26 12:21:38 +0530

    *.py" Run black to reformat all code
    Reformat all the code to be consistent.
    THe only reformatting that has been done is to change single quotes to
    double quotes.

--- configurable_http_proxy/__init__.py
+++ configurable_http_proxy/__init__.py
@@ -6,5 +6,5 @@ log = logging.getLogger("configurable_http_proxy")
 log.setLevel(logging.INFO)
 
 handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('[%(levelname)-.1s %(asctime)s %(name)s] %(message)s'))
+handler.setFormatter(logging.Formatter("[%(levelname)-.1s %(asctime)s %(name)s] %(message)s"))
 logging.root.addHandler(handler)
